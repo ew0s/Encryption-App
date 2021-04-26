0c2f6ab316a1c36293808a5b537f3b75482d7f2e
ew0s <ewos365@mail.ru>
2021-04-26 16:50:10 +0300

    Add: AlgorithmsModels

---
+++ EncryptApp/EncryptApp/Models/Algorithm.swift
@@ -0,0 +1,12 @@
+//
+//  Algorithm.swift
+//  EncryptApp
+//
+//  Created by Егор Савковский on 26.04.2021.
+//
+
+struct Algorithms {
+    let name: String
+    let creator: Person
+    let description: String
+}
