7e59ca3564f492149801d3318f313f0644f662fb
ew0s <ewos365@mail.ru>
2021-04-26 21:03:50 +0300

    Add: algorithm info table view logic

--- EncryptApp/EncryptApp/ViewControllers/AboutAlgorithmsViewController.swift
+++ EncryptApp/EncryptApp/ViewControllers/AboutAlgorithmsViewController.swift
@@ -9,77 +9,53 @@ import UIKit
 
 class AboutAlgorithmsViewController: UITableViewController {
 
+    // MARK: - Private constants
+    private let algorithms = Algorithm.getAlgorithms()
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.setBackground()
+        tableView.rowHeight = 80
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func backButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
     }
 
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        algorithms.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        print(section)
+        return algorithms[section].name
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        2
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "algorithmInfoCell", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        let algorithm = algorithms[indexPath.section]
 
-        // Configure the cell...
+    
+        switch indexPath.row {
+        case 0:
+            content.text = "About algorithm"
+            content.image = UIImage(named: "exclamationMark")
+        default:
+            content.text = algorithm.creator.fullName
+            content.image =  UIImage(named: algorithm.creator.image)
+            content.imageProperties.cornerRadius = tableView.rowHeight / 2
+        }
+
+        cell.contentConfiguration = content
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
