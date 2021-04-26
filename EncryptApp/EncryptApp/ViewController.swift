ae6df87aca65affcd29a0207aa02da3b983f3d3d
ew0s <ewos365@mail.ru>
2021-04-26 08:37:41 +0300

    Add: base project structure

---
+++ EncryptApp/EncryptApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EncryptApp
+//
+//  Created by Егор Савковский on 26.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
